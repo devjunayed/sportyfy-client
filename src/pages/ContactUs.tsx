64e3f0c08eb68d23e05daf73e777fe9f8c973502
Md Junayed <devjunayed@gmail.com>
2025-05-23 12:05:50 +0600

    contact us responsive fix

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -16,16 +16,20 @@ import {
 } from "@ant-design/icons";
 import HeaderSportyfy from "../components/ui/Shared/HeaderSportyfy/HeaderSportyfy";
 
-const { Title, Paragraph } = Typography;
+const {  Paragraph } = Typography;
 
 const ContactUs = () => {
   const onFinish = () => {
   };
   return (
-    <div className="mt-20 max-w-7xl mx-auto">
+    <div className="mt-20 md:max-w-7xl mx-auto ">
+      <div className="mx-2">
+
       <HeaderSportyfy text="Contact us" />
+      </div>
       <div
-        style={{ padding: "50px", backgroundColor: "white", color: "#1B1F3B" }}
+      className="mx-2"
+        style={{ backgroundColor: "white", color: "#1B1F3B" }}
       >
         {/* Contact Us Title */}
         <Row justify="center" style={{ marginBottom: "30px" }}>
@@ -128,9 +132,9 @@ const ContactUs = () => {
         {/* Contact Details */}
         <Row justify="center" style={{ marginTop: "50px" }}>
           <Col span={24} style={{ textAlign: "center", marginBottom: "20px" }}>
-            <Title level={2} style={{ color: "#1B1F3B" }}>
+            <h3  className="text-xl md:text-3xl font-bold" style={{ color: "#1B1F3B" }}>
               Contact Details
-            </Title>
+            </h3>
           </Col>
           <Col span={24} style={{ maxWidth: "600px", margin: "0 auto" }}>
             <Descriptions bordered column={1} size="middle">
@@ -156,9 +160,11 @@ const ContactUs = () => {
         {/* Map Integration */}
         <Row justify="center" style={{ marginTop: "50px" }}>
           <Col span={24} style={{ textAlign: "center", marginBottom: "30px" }}>
-            <Title level={3} style={{ color: "#1B1F3B" }}>
+                <h3  className="text-xl md:text-3xl font-bold mb-10" style={{ color: "#1B1F3B" }}>
               Find Us
-            </Title>
+            </h3>
+            <div className="w-full">
+
             <iframe
               className="w-full"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.1422244872447!2d90.42166437459386!3d23.81354107862736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c62fb95f16c1%3A0xb333248370356dee!2sJamuna%20Future%20Park!5e0!3m2!1sen!2sbd!4v1728321000395!5m2!1sen!2sbd"
@@ -166,6 +172,7 @@ const ContactUs = () => {
               height="450"
               loading="lazy"
             ></iframe>
+            </div>
           </Col>
         </Row>
       </div>
